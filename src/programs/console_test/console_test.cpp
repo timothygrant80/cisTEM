bdbfe06bb80d8b76ed1efbfee877bbaba290b1ab
timothygrant80 <grantt@janelia.hhmi.org>
2015-05-20 14:20:42 +0000

    socket tests

--- src/programs/console_test/console_test.cpp
+++ src/programs/console_test/console_test.cpp
@@ -5,10 +5,12 @@
 #include "wx/socket.h"
 
 #include "../../core/core_headers.h"
+#include "../../core/socket_codes.h"
 
 #define SERVER_ID 100
 #define SOCKET_ID 101
 
+SETUP_SOCKET_CODES
 
 class
 MyApp : public wxAppConsole
@@ -17,10 +19,24 @@ MyApp : public wxAppConsole
 	public:
 		virtual bool OnInit();
 
-		wxSocketClient *m_sock;
+		wxSocketServer *m_server;
+
+
 		bool            m_busy;
 		bool 			is_connected;
 
+		int             m_numClients;
+		int             m_number_watching;
+
+		wxSocketBase *FirstSocket;
+		wxSocketBase *SecondSocket;
+		wxSocketBase *ThirdSocket;
+
+		bool firstsocketisconnected;
+		bool secondsocketisconnected;
+		bool thirdsocketisconnected;
+
+		void OnServerEvent(wxSocketEvent& event);
 		void OnSocketEvent(wxSocketEvent& event);
 
 
@@ -33,225 +49,205 @@ IMPLEMENT_APP(MyApp)
 bool MyApp::OnInit()
 {
 
-	m_sock = new wxSocketClient();
 	wxIPV4address addr;
+	addr.Service(3000);
 
-	// Setup the event handler and subscribe to most events
-	m_sock->SetEventHandler(*this, SOCKET_ID);
-	m_sock->SetNotify(wxSOCKET_CONNECTION_FLAG |wxSOCKET_INPUT_FLAG |wxSOCKET_LOST_FLAG);
-	m_sock->Notify(true);
-	m_busy = false;
+	m_server = new wxSocketServer(addr);
 
-	is_connected = false;
+	// We use Ok() here to see if the server is really listening
+	if (! m_server->Ok())
+	{
+		wxPrintf("Could not listen at the specified port !\n\n");
+		return false;
+	}
+	else
+	{
+		wxPrintf("Server listening.\n\n");
+	}
 
+	// Setup the event handler and subscribe to connection events
+
+	m_server->SetEventHandler(*this, SERVER_ID);
+	m_server->SetNotify(wxSOCKET_CONNECTION_FLAG);
+	m_server->Notify(true);
+
+	this->Connect(SERVER_ID, wxEVT_SOCKET, wxSocketEventHandler( MyApp::OnServerEvent) );
 	this->Connect(SOCKET_ID, wxEVT_SOCKET, wxSocketEventHandler( MyApp::OnSocketEvent) );
 
-	 addr.Hostname("localhost");
-	 addr.Service(3000);
+	m_busy = false;
+	m_numClients = 0;
+	m_number_watching = 0;
 
-	 wxPrintf(_("\nTrying to connect (timeout = 10 sec) ...\n"));
-	 m_sock->Connect(addr, false);
-	 m_sock->WaitOnConnect(10);
+	return true;
+}
 
-	 if (m_sock->IsConnected())
+
+void MyApp::OnServerEvent(wxSocketEvent& event)
+{
+	  wxString s = _("OnServerEvent: ");
+	  wxSocketBase *sock = NULL;
+
+	  switch(event.GetSocketEvent())
 	  {
-	    wxPrintf(_("Succeeded - Connection established!\n\n"));
-		is_connected = true;
-		return true;
+	    case wxSOCKET_CONNECTION : s.Append(_("wxSOCKET_CONNECTION\n")); break;
+	    default                  : s.Append(_("Unexpected event !\n")); break;
+	  }
+
+	  wxPrintf(s);
+
+	  if (m_numClients < 3)
+	  {
+		  // we have space for another
+
+	      // Accept new connection if there is one in the pending
+	      // connections queue, else exit. We use Accept(false) for
+	      // non-blocking accept (although if we got here, there
+	      // should ALWAYS be a pending connection).
+
+	      sock = m_server->Accept(false);
+	      sock->SetFlags(wxSOCKET_WAITALL);//|wxSOCKET_BLOCK);
+
+	      // request identification..
+	    	  wxPrintf(" Requesting identification...\n");
+
+   	  	      sock->WriteMsg(socket_please_identify, SOCKET_CODE_SIZE);
+   	  	      wxPrintf(" Waiting for reply...\n");
+  	  	      sock->WaitForRead(5);
+
+
+   	  	      if (sock->IsData() == true)
+   	  	      {
+   	  	    	  	   sock->ReadMsg(&socket_input_buffer, SOCKET_CODE_SIZE);
+
+
+	    	  	      if (memcmp(socket_input_buffer, socket_identification, sizeof(socket_input_buffer)) != 0)
+	    	  	      {
+	    	  	    	 wxPrintf(" Incorrect MSG - Exiting\n\n");
+	    	  	       	 // incorrect identification - close the connection..
+	    	  	       	 sock->Destroy();
+	    	  	       	 sock = NULL;
+	    	  	      }
+	    	  	      else
+	    	  	      {	  wxPrintf(" Correct Identification\n");
+
+	    	  	    	  sock->SetEventHandler(*this, SOCKET_ID);
+	    	  	    	  sock->SetNotify(wxSOCKET_INPUT_FLAG | wxSOCKET_LOST_FLAG);
+	    	  	    	  sock->Notify(true);
+	    	  	      }
+   	  	      }
+   	  	      else
+   	  	      {
+	    	   	   wxPrintf(" ...Read Timeout \n\n");
+	    	     // time out - close the connection
+	    	   	   sock->Destroy();
+	    	   	   sock = NULL;
+	    	  }
+
+
+	  }
+
+
+/*
+	  if (sock)
+	  {
+	   	    // fill the first free connection..
+
+	   	  if (firstsocketisconnected == false)
+	   	  {
+	   		  FirstSocket = sock;
+	   		  wxPrintf("\n   **   New Connection Accepted  on Socket #1 **\n");
+	   		  m_numClients++;
+	   		  firstsocketisconnected = true;
+	   	  }
+	   	  else
+	   	  if (secondsocketisconnected == false)
+	   	  {
+	   		  SecondSocket = sock;
+	   		  wxPrintf("\n   **   New Connection Accepted  on Socket #2 **\n");
+	   		  m_numClients++;
+	   		  secondsocketisconnected = true;
+	   	  }
+	   	  else
+	   	  if (thirdsocketisconnected == false)
+	   	  {
+	   		  ThirdSocket = sock;
+	   		  wxPrintf("\n   **   New Connection Accepted  on Socket #3 **\n");
+	   		  m_numClients++;
+	   		  thirdsocketisconnected = true;
+	   	  }
+	   	  else
+	   	  {
+	   		wxPrintf("\n   **   Error: Can't find a free socket, though i think there should be one?   **\n");
+
+	   	  }
+
 	  }
 	  else
 	  {
-		m_sock->Close();
-		wxPrintf(_("Failed ! Unable to connect\n"));
-		return false;
+		  wxPrintf("\n   **   Error: couldn't accept a new connection, maybe I have 3 connected already?   **\n");
+	    return;
 	  }
-}
 
+*/
+
+
+	  //UpdateStatusBar();
+}
 
 void MyApp::OnSocketEvent(wxSocketEvent& event)
 {
   wxString s = _("OnSocketEvent: ");
+  wxSocketBase *sock = event.GetSocket();
 
-  char side[10];
-  char system_command[500];
-
-  if (event.GetSocketEvent() == wxSOCKET_INPUT)
+  // First, print a message
+  switch(event.GetSocketEvent())
   {
-		// we need to package the bet to send into a stream of bytes..
-
-	 	// byte 1 = home (0), draw(1) or away(2);
-		// byte 2,3,4,5 = pounds to bet
-		// byte 6 = pence to bet
-		// byte 7,8,9,10 = expected_nominator
-		// byte 11,12,13,14 = expected_denominator
-	    // byte 15 = bet365 (0), Bwin (1), VC (2);
-
-	  	int side_to_bet;
-		int pounds_to_bet;
-		int pence_to_bet;
-		int nominator;
-		int denominator;
-		int site_to_bet;
-
-		int temp_int;
-		char *char_pointer = (char*) &temp_int;
-
-		float money_to_bet;
-
-		wxDateTime now = wxDateTime::Now();
-
-	  	unsigned char c[15];
-	  	m_sock->Read(&c, 15);
-
-	  	side_to_bet = int(c[0]);
-
-	  	char_pointer[0] = c[1];
-	  	char_pointer[1] = c[2];
-	  	char_pointer[2] = c[3];
-	  	char_pointer[3] = c[4];
-
-	  	pounds_to_bet = temp_int;
-
-	  	pence_to_bet = int(c[5]);
-
-	  	char_pointer[0] = c[6];
-	  	char_pointer[1] = c[7];
-	  	char_pointer[2] = c[8];
-	  	char_pointer[3] = c[9];
-
-	  	nominator = temp_int;
-
-	  	char_pointer[0] = c[10];
-	  	char_pointer[1] = c[11];
-	  	char_pointer[2] = c[12];
-	  	char_pointer[3] = c[13];
-
-	  	denominator = temp_int;
-
-	  	site_to_bet = int(c[14]);
-
-	  	wxPrintf(wxT("  **   Bet received at "));
-	  	wxPrintf(wxString::Format(wxT("%s   **\n"), now.Format().c_str()));
-
-	  	wxPrintf(wxString::Format(_("Betting %c%d.%d on "), 0xA3, pounds_to_bet, pence_to_bet));
-
-	  	money_to_bet = float(pounds_to_bet) + (float(pence_to_bet) / 100);
-
-	  	if (side_to_bet == 0)
-	  	{
-	  		wxPrintf(wxT("HOME\n"));
-	  		sprintf(side, "home");
-	  	}
-	  	else
-	  	if (side_to_bet == 1)
-	  	{
-	  		wxPrintf(wxT("DRAW\n"));
-	  		sprintf(side, "draw");
-	  	}
-	  	else
-	  	if (side_to_bet == 2)
-	  	{
-	  		wxPrintf(wxT("AWAY\n"));
-	  		sprintf(side, "away");
-	  	}
-
-	  	wxPrintf(wxString::Format((wxT("Expected Odds Are %d/%d\n")), nominator, denominator));
-
-	  	// now call Andy's script appropriately...
-
-	  	if (site_to_bet == 0)
-	  	{
-	  		wxPrintf(wxT("Betting at BET365\n\n"));
-	  		sprintf(system_command, "\"Z:\\VM Scripts\\bet365.ahk\" %s %.2f", side, money_to_bet);
-	  	}
-	  	else
-	  	if (site_to_bet == 1)
-	  	{
-	  		wxPrintf(wxT("Betting at BWIN\n\n"));
-	  		sprintf(system_command, "\"Z:\\VM Scripts\\bwin.ahk\" %s %.2f", side, money_to_bet);
-	  	}
-	  	else
-	  	if (site_to_bet == 2)
-	  	{
-	  		wxPrintf(wxT("Betting at VC\n\n"));
-	  		sprintf(system_command, "\"Z:\\VM Scripts\\vc.ahk\" %s %.2f", side, money_to_bet);
-	  	}
-
-	  	system(system_command);
-
-	  	//wxExecute(wxT(system_command));
-
-
-	  	wxPrintf(wxT("Running the following script :-\n\n"));
-
+    case wxSOCKET_INPUT : s.Append(_("wxSOCKET_INPUT\n")); break;
+    case wxSOCKET_LOST  : s.Append(_("wxSOCKET_LOST\n")); break;
+    default             : s.Append(_("Unexpected event !\n")); break;
   }
-  else
-  if (event.GetSocketEvent() == wxSOCKET_LOST)
-  {
-	  is_connected = false;
-	  wxPrintf(wxT("Connection Lost!!\n\n"));
-	  ExitMainLoop();
 
+  //m_text->AppendText(s);
+
+  wxPrintf(s);
+
+  // Now we process the event
+  switch(event.GetSocketEvent())
+  {
+    case wxSOCKET_INPUT:
+    {
+      // We disable input events, so that the test doesn't trigger
+      // wxSocketEvent again.
+      sock->SetNotify(wxSOCKET_LOST_FLAG);
+/*
+      // Which test are we going to run?
+
+      switch (c)
+      {
+        case 0xBE:  wxPrintf("0xBE\n\n"); break;//Test1(sock); break;
+        case 0xCE:  wxPrintf("0xCE\n\n"); break;//Test2(sock); break;
+        case 0xDE:  wxPrintf("0xDE\n\n");break;//Test3(sock); break;
+        default:
+          wxPrintf("Unknown test id received from client\n\n");
+      }
+
+*/
+      // Enable input events again.
+
+      sock->SetNotify(wxSOCKET_LOST_FLAG | wxSOCKET_INPUT_FLAG);
+      break;
+    }
+
+    case wxSOCKET_LOST:
+    {
+        //m_numClients--;
+
+        //wxPrintf("Socket Disconnected!!\n");
+        sock->Destroy();
+
+        break;
+    }
+    default: ;
   }
 }
-
-
-
-/*
-class
-MyApp : public wxAppConsole
-{
-
-	public:
-		virtual bool OnInit();
-
-		wxSocketClient *m_sock;
-		bool            m_busy;
-		bool 			is_connected;
-
-		void OnSocketEvent(wxSocketEvent& event);
-
-
-};
-
-
-IMPLEMENT_APP(MyApp)
-
-
-bool MyApp::OnInit()
-{
-
-	m_sock = new wxSocketClient(wxSOCKET_BLOCK);
-	wxIPV4address addr;
-
-	// Setup the event handler and subscribe to most events
-	//m_sock->SetEventHandler(*this, SOCKET_ID);
-	//m_sock->SetNotify(wxSOCKET_CONNECTION_FLAG |wxSOCKET_INPUT_FLAG |wxSOCKET_LOST_FLAG);
-	//m_sock->Notify(true);
-	//m_busy = false;
-
-	is_connected = false;
-
-	//this->Connect(SOCKET_ID, wxEVT_SOCKET, wxSocketEventHandler( MyApp::OnSocketEvent) );
-
-	 addr.Hostname("localhost");
-	 addr.Service(3000);
-
-	 wxPrintf(_("\nTrying to connect (timeout = 10 sec) ...\n"));
-	 m_sock->Connect(addr, false);
-	 m_sock->WaitOnConnect(10);
-
-	 if (m_sock->IsConnected())
-	  {
-	    wxPrintf(_("Succeeded - Connection established!\n\n"));
-		is_connected = true;
-		return true;
-	  }
-	  else
-	  {
-		m_sock->Close();
-		wxPrintf(_("Failed ! Unable to connect\n"));
-		return false;
-	  }
-}*/
 

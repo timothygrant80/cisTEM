964078921ba4a3feea99ccdce6c55957fc71d97a
arohou <rohoua@gene.com>
2016-07-05 12:56:07 +0000

    remove_outlier_pixels - added option to zero-float and normalize following outlier resetting

--- src/programs/remove_outlier_pixels/remove_outlier_pixels.cpp
+++ src/programs/remove_outlier_pixels/remove_outlier_pixels.cpp
@@ -24,14 +24,15 @@ void NikoTestApp::DoInteractiveUserInput()
 
 	UserInput *my_input = new UserInput("RemoveOutlierPixels", 1.0);
 
-	std::string input_filename		=		my_input->GetFilenameFromUser("Input image file name", "Filename of input image", "input.mrc", true );
-	std::string output_filename		=		my_input->GetFilenameFromUser("Output image file name", "Filename of output image", "output.mrc", false );
-	float	 	num_sigmas			=		my_input->GetFloatFromUser("Number of standard deviations","Pixels more than this number of standard deviations above or below the mean will be reset to the mean","6.0",0.0f);
+	std::string input_filename				=		my_input->GetFilenameFromUser("Input image file name", "Filename of input image", "input.mrc", true );
+	std::string output_filename				=		my_input->GetFilenameFromUser("Output image file name", "Filename of output image", "output.mrc", false );
+	float	 	num_sigmas					=		my_input->GetFloatFromUser("Number of standard deviations","Pixels more than this number of standard deviations above or below the mean will be reset to the mean","6.0",0.0f);
+	bool		zero_float_and_normalize	=		my_input->GetYesNoFromUser("Also zero-float and normalize?","After outlier pixels have been removed, zero-float and normalize images","no");
 
 	delete my_input;
 
-	my_current_job.Reset(19);
-	my_current_job.ManualSetArguments("ttf",input_filename.c_str(), output_filename.c_str(), num_sigmas);
+	my_current_job.Reset(4);
+	my_current_job.ManualSetArguments("ttfb",input_filename.c_str(), output_filename.c_str(), num_sigmas,zero_float_and_normalize);
 }
 
 // override the do calculation method which will be what is actually run..
@@ -42,6 +43,7 @@ bool NikoTestApp::DoCalculation()
 	std::string	input_filename 						= my_current_job.arguments[0].ReturnStringArgument();
 	std::string	output_filename 					= my_current_job.arguments[1].ReturnStringArgument();
 	float		num_sigmas							= my_current_job.arguments[2].ReturnFloatArgument();
+	bool		zero_float_and_normalize			= my_current_job.arguments[3].ReturnBoolArgument();
 
 
 	MRCFile my_input_file(input_filename,false);
@@ -57,6 +59,10 @@ bool NikoTestApp::DoCalculation()
 		my_progress.Update(image_counter + 1);
 		my_image.ReadSlice(&my_input_file,image_counter+1);
 		my_image.ReplaceOutliersWithMean(num_sigmas);
+		if (zero_float_and_normalize)
+		{
+			my_image.ZeroFloatAndNormalize();
+		}
 		my_image.WriteSlice(&my_output_file,image_counter+1);
 	}
 

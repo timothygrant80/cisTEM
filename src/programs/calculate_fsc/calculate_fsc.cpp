126ffef499c6dc2b7984491e5d9259502b149dda
Nikolaus Grigorieff <ngrigorieff@users.noreply.github.com>
2017-11-15 11:03:34 -0500

    CalculateFSC: corrected description for output resolution statistics in input section, added inner mask option.

--- src/programs/calculate_fsc/calculate_fsc.cpp
+++ src/programs/calculate_fsc/calculate_fsc.cpp
@@ -25,17 +25,18 @@ void CalculateFSC::DoInteractiveUserInput()
 	wxString output_reconstruction_1	= my_input->GetFilenameFromUser("Input reconstruction 1", "The first input 3D reconstruction used for FSC calculation", "my_reconstruction_1.mrc", false);
 	wxString output_reconstruction_2	= my_input->GetFilenameFromUser("Input reconstruction 2", "The second input 3D reconstruction used for FSC calculation", "my_reconstruction_2.mrc", false);
 	wxString input_mask					= my_input->GetFilenameFromUser("Input mask file name", "Name of input 3D volume to be applied to input reconstructions ", "mask.mrc", false );
-	wxString res_statistics				= my_input->GetFilenameFromUser("Input data statistics", "The table listing FSC, Part_FSC, Part_SSNR and Rec_SSNR", "my_statistics.txt", false);
+	wxString res_statistics				= my_input->GetFilenameFromUser("Output resolution statistics", "The table listing FSC, Part_FSC, Part_SSNR and Rec_SSNR", "my_statistics.txt", false);
 	float pixel_size					= my_input->GetFloatFromUser("Pixel size (A)", "Pixel size of the map in Angstroms", "1.0", 0.000001);
-	float mask_radius					= my_input->GetFloatFromUser("Mask radius (A)", "Radius of mask to be applied to the input reconstructions, in Angstroms", "100.0", 0.0);
+	float inner_mask_radius				= my_input->GetFloatFromUser("Inner mask radius (A)", "Radius of a circular mask to be applied to the center of the input reconstructions, in Angstroms", "0.0", 0.0);
+	float outer_mask_radius				= my_input->GetFloatFromUser("Outer mask radius (A)", "Radius of a circular mask to be applied to the input reconstructions, in Angstroms", "100.0", inner_mask_radius);
 	float molecular_mass_in_kDa			= my_input->GetFloatFromUser("Molecular mass of particle (kDa)", "Total molecular mass of the particle to be reconstructed in kilo Daltons", "1000.0", 0.0);
 	bool use_mask						= my_input->GetYesNoFromUser("Use 3D mask", "Should the 3D mask be used to mask the input reconstructions before FSC calculation?", "No");
 
 	delete my_input;
 
-	my_current_job.Reset(8);
-	my_current_job.ManualSetArguments("ttttfffb", output_reconstruction_1.ToUTF8().data(), output_reconstruction_2.ToUTF8().data(), input_mask.ToUTF8().data(), res_statistics.ToUTF8().data(),
-			pixel_size, mask_radius, molecular_mass_in_kDa, use_mask);
+	my_current_job.Reset(9);
+	my_current_job.ManualSetArguments("ttttffffb", output_reconstruction_1.ToUTF8().data(), output_reconstruction_2.ToUTF8().data(), input_mask.ToUTF8().data(), res_statistics.ToUTF8().data(),
+			pixel_size, inner_mask_radius, outer_mask_radius, molecular_mass_in_kDa, use_mask);
 }
 
 // override the do calculation method which will be what is actually run..
@@ -48,9 +49,10 @@ bool CalculateFSC::DoCalculation()
 	wxString input_mask					= my_current_job.arguments[2].ReturnStringArgument();
 	wxString res_statistics				= my_current_job.arguments[3].ReturnStringArgument();
 	float pixel_size					= my_current_job.arguments[4].ReturnFloatArgument();
-	float mask_radius					= my_current_job.arguments[5].ReturnFloatArgument();
-	float molecular_mass_in_kDa			= my_current_job.arguments[6].ReturnFloatArgument();
-	bool use_mask						= my_current_job.arguments[7].ReturnBoolArgument();
+	float inner_mask_radius				= my_current_job.arguments[5].ReturnFloatArgument();
+	float outer_mask_radius				= my_current_job.arguments[6].ReturnFloatArgument();
+	float molecular_mass_in_kDa			= my_current_job.arguments[7].ReturnFloatArgument();
+	bool use_mask						= my_current_job.arguments[8].ReturnBoolArgument();
 
 	MRCFile reconstruction_1(output_reconstruction_1.ToStdString(), false);
 	MRCFile reconstruction_2(output_reconstruction_2.ToStdString(), false);
@@ -107,8 +109,8 @@ bool CalculateFSC::DoCalculation()
 	}
 	else
 	{
-		mask_volume_in_voxels = density_map_1.CosineMask(mask_radius / pixel_size, cosine_edge / pixel_size);
-		mask_volume_in_voxels = density_map_2.CosineMask(mask_radius / pixel_size, cosine_edge / pixel_size);
+		mask_volume_in_voxels = density_map_1.CosineRingMask(inner_mask_radius / pixel_size, outer_mask_radius / pixel_size, cosine_edge / pixel_size);
+		mask_volume_in_voxels = density_map_2.CosineRingMask(inner_mask_radius / pixel_size, outer_mask_radius / pixel_size, cosine_edge / pixel_size);
 	}
 
 	density_map_1.ForwardFFT();

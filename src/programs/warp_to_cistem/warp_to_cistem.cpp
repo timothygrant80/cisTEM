e5ebffa0982435ab8a84f8a1e994b9f511bda816
Benjamin Barad <benjamin.barad@gmail.com>
2019-08-19 10:49:36 -0700

    ctf import

--- src/programs/warp_to_cistem/warp_to_cistem.cpp
+++ src/programs/warp_to_cistem/warp_to_cistem.cpp
@@ -10,7 +10,7 @@ WarpToCistemApp : public MyApp
 	bool DoCalculation();
 	MovieAsset LoadMovieFromWarp(wxXmlDocument warp_doc, wxString warp_folder, wxString movie_filename, unsigned long count, float wanted_binned_pixel_size);
 	ImageAsset LoadImageFromWarp(wxString image_filename, unsigned long parent_asset_id, double parent_voltage, double parent_cs, bool parent_are_white);
-
+	CTF LoadCTFFromWarp(wxXmlDocument warp_doc, float pixel_size, float voltage, float spherical_aberration, wxString wanted_avrot_filename);
 	void DoInteractiveUserInput();
 
 	wxString warp_directory;
@@ -18,6 +18,8 @@ WarpToCistemApp : public MyApp
 	wxString project_name;
 	float wanted_binned_pixel_size;
 	bool do_import_images;
+	bool do_import_ctf_results;
+//	bool do_import_particle_coordinates;
 	Project new_project;
 	private:
 };
@@ -31,7 +33,9 @@ void WarpToCistemApp::DoInteractiveUserInput()
 	wxString cistem_parent_directory = "";
 	wxString project_name = "";
 	float wanted_binned_pixel_size = 1.0;
-	bool do_import_images = true;
+	bool do_import_images = false;
+	bool do_import_ctf_results = false;
+//	bool do_import_particle_coordinates = true;
 	UserInput *my_input = new UserInput("Warp to Cistem", 1.0);
 
 	warp_directory = my_input->GetFilenameFromUser("Input Warp Directory", "The folder in which Warp processed movies", "./Data", false);
@@ -39,9 +43,13 @@ void WarpToCistemApp::DoInteractiveUserInput()
 	project_name = my_input -> GetFilenameFromUser("Project Name", "Name for new cisTEM2 project", "New_Project", false);
 	wanted_binned_pixel_size = my_input -> GetFloatFromUser("Binned Pixel Size", "Pixel size to resample movies to after import.", "1.0", 0.0);
 	do_import_images = my_input -> GetYesNoFromUser("Import Images?", "Should we import aligned averaged images from WARP (using a different motion correction system than cisTEM)?", "Yes");
+	if (do_import_images) {
+		do_import_ctf_results = my_input -> GetYesNoFromUser("Import CTF Estimates?", "Should we import results of CTF estimation from Warp?", "Yes");
+	} else do_import_ctf_results=false;
+		//	do_import_particle_coordinates = my_input -> GetYesNoFromUser("Import Particle Coordinates?", "Should we import coordinates of particles picked by WARP (using BoxNet)?", "Yes");
 	delete my_input;
 
-	my_current_job.ManualSetArguments("tttbf",	warp_directory.ToUTF8().data(), cistem_parent_directory.ToUTF8().data(), project_name.ToUTF8().data(), do_import_images, wanted_binned_pixel_size);
+	my_current_job.ManualSetArguments("tttbfb",	warp_directory.ToUTF8().data(), cistem_parent_directory.ToUTF8().data(), project_name.ToUTF8().data(), do_import_images, wanted_binned_pixel_size, do_import_ctf_results);
 
 }
 
@@ -150,6 +158,117 @@ ImageAsset WarpToCistemApp::LoadImageFromWarp(wxString image_filename, unsigned 
 	return new_asset;
 }
 
+CTF WarpToCistemApp::LoadCTFFromWarp(wxXmlDocument warp_doc, float pixel_size, float voltage, float spherical_aberration, wxString wanted_avrot_filename) {
+	double defocus = 0.0;
+	wxString str_defocus;
+	double defocus_delta = 0.0;
+	wxString str_defocus_delta;
+	double defocus_angle = 0.0;
+	wxString str_defocus_angle;
+	double defocus_1 = 0.0;
+	double defocus_2 = 0.0;
+//	double defocus_max = 2.0;
+//	wxString str_defocus_max;
+//	double defocus_min = 0.0;
+//	wxString str_defocus_min;
+	double amplitude_contrast = 0.07;
+	wxString str_amplitude_contrast;
+	double minimum_range = .1;
+	double minimum_frequency = .1;
+	wxString str_minimum_range;
+	double maximum_range = 0.6;
+	double maximum_frequency = 0.6;
+	wxString str_maximum_range;
+
+	double phase_shift = 0.0;
+	wxString str_phase_shift;
+	double resolution_estimate = 0.0;
+	wxString str_resolution_estimate;
+
+	str_resolution_estimate = warp_doc.GetRoot()->GetAttribute("CTFResolutionEstimate");
+	if(!str_resolution_estimate.ToDouble(&resolution_estimate)) {SendErrorAndCrash("Couldn't convert Voltage to a double");}
+	wxXmlNode *child_1 = warp_doc.GetRoot()->GetChildren();
+	while (child_1) {
+		// TODO - parse `PS1D` eventually... but not yet.
+		if ((child_1)->GetName() == "OptionsCTF") {
+			wxXmlNode *child_2 = child_1->GetChildren();
+			while (child_2) {
+				if (child_2->GetAttribute("Name") == "Amplitude") {
+					str_amplitude_contrast = child_2->GetAttribute("Value");
+					if(!str_amplitude_contrast.ToDouble(&amplitude_contrast)) {SendErrorAndCrash("Couldn't convert Amplitude to a double");}
+				}
+				if (child_2->GetAttribute("Name") == "RangeMax") {
+					str_maximum_range = child_2->GetAttribute("Value");
+					if(!str_maximum_range.ToDouble(&maximum_range)) {SendErrorAndCrash("Couldn't convert RangeMax to a double");}
+					maximum_frequency = maximum_range / (2*pixel_size);
+
+				}
+				if (child_2->GetAttribute("Name") == "RangeMin") {
+					str_minimum_range = child_2->GetAttribute("Value");
+					if(!str_minimum_range.ToDouble(&minimum_range)) {SendErrorAndCrash("Couldn't convert RangeMin to a double");}
+					minimum_frequency = minimum_range / (2*pixel_size);
+				}
+				child_2 = child_2->GetNext();
+			}
+		}
+		else if ((child_1)->GetName() == "CTF"){
+			wxXmlNode *child_2 = child_1->GetChildren();
+			while (child_2) {
+				if (child_2->GetAttribute("Name") == "DefocusAngle") {
+					str_defocus_angle = child_2->GetAttribute("Value");
+					if(!str_defocus_angle.ToDouble(&defocus_angle)) {SendErrorAndCrash("Couldn't convert DefocusAngle to a double");}
+				}
+				if (child_2->GetAttribute("Name") == "Defocus") {
+					str_defocus = child_2->GetAttribute("Value");
+					if(!str_defocus.ToDouble(&defocus)) {SendErrorAndCrash("Couldn't convert Defocus to a double");}
+				}
+				if (child_2->GetAttribute("Name") == "DefocusDelta") {
+					str_defocus_delta = child_2->GetAttribute("Value");
+					if(!str_defocus_delta.ToDouble(&defocus_delta)) {SendErrorAndCrash("Couldn't convert DefocusDelta to a double");}
+				}
+				if (child_2->GetAttribute("Name") == "PhaseShift") {
+					str_phase_shift = child_2->GetAttribute("Value");
+					if(!str_phase_shift.ToDouble(&phase_shift)) {SendErrorAndCrash("Couldn't convert RangeMin to a double");}
+				}
+				child_2 = child_2->GetNext();
+			}
+		}
+		child_1 = child_1->GetNext();
+	}
+
+	defocus_1 = (defocus + defocus_delta/2.0)*10000; // convert to Å, warp uses um
+	defocus_2 = (defocus - defocus_delta/2.0)*10000; // convert to Å, warp uses um
+	if (defocus_angle >90) defocus_angle += -180; // convert from 0 to 180 to -90 to 90
+	CTF return_ctf = CTF(voltage,
+			spherical_aberration,
+			(float) amplitude_contrast,
+			(float) defocus_1,
+			(float) defocus_2,
+			(float) defocus_angle,
+			(float) minimum_frequency,
+			(float) maximum_frequency,
+			-1.0f,
+			pixel_size,
+			(float) phase_shift,
+			0.0f,
+			0.0f,
+			0.0f,
+			0.0f); //A
+	return_ctf.SetHighestFrequencyWithGoodFit(1.0/resolution_estimate);
+
+	// Write out the avrot file here so we have it in the same scope as the xmldoc for when we want to actually scrape the plot.
+	wxTextFile *avrot_file = new wxTextFile(wanted_avrot_filename);
+	// Stub file - all 1 all the time
+	avrot_file->AddLine(wxString("# This file is a stub that does not reflect the data from Warp"));
+	for (long counter=0; counter<6; counter++) {
+		avrot_file->AddLine(wxString("1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0"));
+	}
+	avrot_file->Write();
+	avrot_file->Close();
+
+	return return_ctf;
+}
+
 bool WarpToCistemApp::DoCalculation()
 {
 	wxString warp_directory = my_current_job.arguments[0].ReturnStringArgument();
@@ -157,6 +276,7 @@ bool WarpToCistemApp::DoCalculation()
 	wxString project_name = my_current_job.arguments[2].ReturnStringArgument();
 	bool do_import_images = my_current_job.arguments[3].ReturnBoolArgument();
 	float wanted_binned_pixel_size = my_current_job.arguments[4].ReturnFloatArgument();
+	bool do_import_ctf_results = my_current_job.arguments[5].ReturnBoolArgument();
 
 	ProgressBar *my_progress;
 	long counter;
@@ -194,6 +314,8 @@ bool WarpToCistemApp::DoCalculation()
 	MovieAsset new_movie_asset;
 	ImageAssetList image_list = ImageAssetList();
 	ImageAsset new_image_asset;
+	std::vector<CTF> ctf_list;
+	CTF new_ctf_asset;
 	if (all_files.IsEmpty() == true) {SendErrorAndCrash("No movies were detected in the warp directory.");}
 	my_progress = new ProgressBar(number_of_files);
 
@@ -213,6 +335,14 @@ bool WarpToCistemApp::DoCalculation()
 			image_filename = warp_directory + "average/" + wxFileName(xml_filename).GetName() + ".mrc";
 			if (do_import_images && wxFileName(image_filename).IsOk() == true && wxFileName(image_filename).FileExists() == true) {
 				new_image_asset = LoadImageFromWarp(image_filename, new_movie_asset.asset_id, new_movie_asset.microscope_voltage, new_movie_asset.spherical_aberration, new_movie_asset.protein_is_white);
+				// Need to do the ctf first to adjust the ctf_estimation_id if necessary
+				if (do_import_ctf_results) {
+					new_image_asset.ctf_estimation_id = new_image_asset.asset_id;
+					wxString wanted_avrot_filename = wanted_folder_name + wxString::Format("/Assets/CTF/%s_CTF_0_avrot.txt", new_image_asset.asset_name);
+
+					CTF new_ctf_asset = LoadCTFFromWarp(doc, new_image_asset.pixel_size, new_movie_asset.microscope_voltage, new_movie_asset.spherical_aberration, wanted_avrot_filename);
+					ctf_list.push_back(new_ctf_asset);
+				}
 				image_list.AddAsset(&new_image_asset);
 			} else if (do_import_images) wxPrintf("Couldn't find averaged image: %s\n", image_filename);
 
@@ -288,6 +418,7 @@ bool WarpToCistemApp::DoCalculation()
 			my_progress->Update(counter + 1);
 		}
 		new_project.database.EndBatchInsert();
+
 		delete my_progress;
 
 		my_progress = new ProgressBar(image_list.number_of_assets);
@@ -305,6 +436,90 @@ bool WarpToCistemApp::DoCalculation()
 			new_project.database.EndBatchInsert();
 			my_progress->Update(counter+1);
 		}
+
+		if (do_import_ctf_results) {
+			wxPrintf("\nInserting CTF results in database\n\n");
+			wxDateTime now = wxDateTime::Now();
+			my_progress = new ProgressBar(image_list.number_of_assets);
+			new_project.database.BeginBatchInsert("ESTIMATED_CTF_PARAMETERS", 32,
+												  "CTF_ESTIMATION_ID",
+												  "CTF_ESTIMATION_JOB_ID",
+												  "DATETIME_OF_RUN",
+												  "IMAGE_ASSET_ID",
+												  "ESTIMATED_ON_MOVIE_FRAMES",
+												  "VOLTAGE",
+												  "SPHERICAL_ABERRATION",
+												  "PIXEL_SIZE",
+												  "AMPLITUDE_CONTRAST",
+												  "BOX_SIZE",
+												  "MIN_RESOLUTION",
+												  "MAX_RESOLUTION",
+												  "MIN_DEFOCUS",
+												  "MAX_DEFOCUS",
+												  "DEFOCUS_STEP",
+												  "RESTRAIN_ASTIGMATISM",
+												  "TOLERATED_ASTIGMATISM",
+												  "FIND_ADDITIONAL_PHASE_SHIFT",
+												  "MIN_PHASE_SHIFT",
+												  "MAX_PHASE_SHIFT",
+												  "PHASE_SHIFT_STEP",
+												  "DEFOCUS1",
+												  "DEFOCUS2",
+												  "DEFOCUS_ANGLE",
+												  "ADDITIONAL_PHASE_SHIFT",
+												  "SCORE",
+												  "DETECTED_RING_RESOLUTION",
+												  "DETECTED_ALIAS_RESOLUTION",
+												  "OUTPUT_DIAGNOSTIC_FILE",
+												  "NUMBER_OF_FRAMES_AVERAGED",
+												  "LARGE_ASTIGMATISM_EXPECTED",
+												  "ICINESS");
+			for	(counter = 0; counter < image_list.number_of_assets; counter++) {
+				new_image_asset = reinterpret_cast <ImageAsset *> (image_list.assets)[counter];
+				CTF new_ctf = ctf_list.at(counter);
+				wxString wanted_ctf_filename = wanted_folder_name + wxString::Format("/Assets/CTF/%s_CTF_0.mrc", new_image_asset.asset_name); // This logic is repeated before - its the only case of straight repeating code I've resorted to, but calculating it twice is easier than storing the data in an object somewhere for multiple iterations.
+				new_project.database.AddToBatchInsert("iiliirrrrirrrrririrrrrrrrrrrtiir",
+													new_image_asset.ctf_estimation_id, // Image asset join column reference
+													1, // CTF Job id
+													(long int) now.GetAsDOS(), // datetime
+													new_image_asset.asset_id, // Image asset parent
+													1, // Estimated on movie frames - I am not scraping this from Warp because it doesn't fit in the CTF object easily, and doesn't add enough to be worth adding to the object definition.
+													new_image_asset.microscope_voltage, // I could get this from the wavelength of the CTF object, but it doesn't seem necessary.
+													new_ctf.GetSphericalAberration() /10000000.0*new_image_asset.pixel_size, // convert back to mm
+													new_image_asset.pixel_size,
+													new_ctf.GetAmplitudeContrast(),
+													512, // box size - I think this is the box size of the spectrum and am filling on that assumption.
+													new_image_asset.pixel_size/new_ctf.GetLowestFrequencyForFitting(), // Invert the frequency
+													new_image_asset.pixel_size/new_ctf.GetHighestFrequencyForFitting(), // Invert the frequency
+													10000*0.0, // Min defocus - I am not scraping this from warp (same as ESTIMATED_ON_MOVIE_FRAMES) but these are the defaults as of 190815
+													10000*2.0, // Max defocus - I am not scraping this from warp (same as above).
+													-1, // Defocus Step is not reported by warp anywhere... this is negative for safety.
+													0, // Restrain Astigmatism is not selectable by warp.
+													-1, //Tolerated Astigmatism is not selectable by warp.
+													0, // Not scraping find additional phase shift - See above, I don't want to add all this to CTF unless I have to.
+													0.0, // Min Phase
+													0.0, // Max Phase
+													0.0, // Phase shift step
+													new_ctf.GetDefocus1()*new_image_asset.pixel_size,
+													new_ctf.GetDefocus2()*new_image_asset.pixel_size,
+													new_ctf.GetAstigmatismAzimuth()*180/PI, // Convert back to degrees
+													new_ctf.GetAdditionalPhaseShift(),
+													-1.0, // Score
+													1.0/new_ctf.GetHighestFrequencyWithGoodFit(), //Resolution of fit.
+													0.0, // Alias Resolution
+													wanted_ctf_filename.ToStdString().c_str(),
+													-1, // Number of frames averaged
+													0, // Large Astigmatism Expected is not used by warp
+													-1.0); // Iciness is not calculated by warp
+
+				my_progress->Update(counter+1);
+			}
+			delete my_progress;
+			new_project.database.EndBatchInsert();
+		}
+
+
+		new_project.database.Commit();
 
 		wxPrintf("\nDone with database operations\n\n");
 
@@ -346,6 +561,11 @@ bool WarpToCistemApp::DoCalculation()
 			buffer_image.ComputeAverageAndSigmaOfValuesInSpectrum(float(buffer_image.logical_x_dimension)*0.5,float(buffer_image.logical_x_dimension),average,sigma,12);
 			buffer_image.SetMinimumAndMaximumValues(average - 15.0, average + 15.0);
 			buffer_image.QuickAndDirtyWriteSlice(wanted_spectrum_filename.ToStdString(), 1);
+			// Also write fake diagnostic ctf spectrum in this scope to reuse the work done above.
+			if (do_import_ctf_results) {
+				wxString wanted_ctf_filename = wanted_folder_name + wxString::Format("/Assets/CTF/%s_CTF_0.mrc", new_image_asset.asset_name);
+				buffer_image.QuickAndDirtyWriteSlice(wanted_ctf_filename.ToStdString(), 1,true);
+			}
 
 			//Scaled Image - borrowed the logic heavily from Unblur
 			wxString wanted_scaled_filename = wanted_folder_name + wxString::Format("/Assets/Images/Scaled/%s.mrc", new_image_asset.asset_name); //, new_image_asset.asset_id, 0); _%i_%i
@@ -359,12 +579,13 @@ bool WarpToCistemApp::DoCalculation()
 			large_image.ClipInto(&buffer_image);
 			buffer_image.BackwardFFT();
 			buffer_image.QuickAndDirtyWriteSlice(wanted_scaled_filename.ToStdString(), 1,true);
+
 			my_progress->Update(counter+1);
 		}
 		delete my_progress;
 		wxPrintf("\nDone writing spectra and scaled images\n\n");
 	}
-	new_project.database.Commit();
+
 	wxPrintf("\ncisTEM project ready to be loaded by GUI.\n");
 	return true;
 }

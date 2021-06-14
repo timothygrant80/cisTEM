4150a747109474c55603b1823d9f1821ec4ebc2e
Will Woods <willwoods@Wills-MacBook-Pro.local>
2021-06-14 21:41:03 +0000

    it runs

--- src/programs/generate_local_res/generate_local_res.cpp
+++ src/programs/generate_local_res/generate_local_res.cpp
@@ -11,7 +11,7 @@ Generate_Local_Res_App : public MyApp
 	private:
 };
 
-//IMPLEMENT_APP(Generate_Local_Res_App)
+IMPLEMENT_APP(Generate_Local_Res_App)
 
 void Generate_Local_Res_App::DoInteractiveUserInput()
 {
@@ -37,6 +37,7 @@ void Generate_Local_Res_App::DoInteractiveUserInput()
 	outer_mask_radius = my_input->GetFloatFromUser("Outer mask radius (A)", "Radius of a circular mask to be applied to the final reconstruction in Angstroms", "100.0", inner_mask_radius);
     molecular_mass_kDa = my_input->GetFloatFromUser("Molecular mass of particle (kDa)", "Total molecular mass of the particle to be reconstructed in kilo Daltons", "1000.0", 0.0);
     symmetry = my_input->GetSymmetryFromUser("Particle symmetry", "The symmetry imposed on the input reconstructions", "C1");
+	pixel_size = my_input->GetFloatFromUser("Pixel Size", "Pixel Size of the image", "1.50", 1.50);
 
 	#ifdef _OPENMP
 	number_of_threads = my_input->GetIntFromUser("Max. threads to use for calculation", "When threading, what is the max threads to run", "1", 1);
@@ -46,7 +47,7 @@ void Generate_Local_Res_App::DoInteractiveUserInput()
 
     delete my_input;
 
-    my_current_job.ManualSetArguments("ttiifffti", half_map_1.ToUTF8().data(), half_map_2.ToUTF8().data(), first_slice, last_slice, inner_mask_radius, outer_mask_radius, molecular_mass_kDa, symmetry, number_of_threads);
+    my_current_job.ManualSetArguments("ttiiffftfi", half_map_1.ToUTF8().data(), half_map_2.ToUTF8().data(), first_slice, last_slice, inner_mask_radius, outer_mask_radius, molecular_mass_kDa, symmetry, pixel_size, number_of_threads);
 
 }
 
@@ -65,57 +66,79 @@ bool Generate_Local_Res_App::DoCalculation()
     float outer_mask_radius     = my_current_job.arguments[5].ReturnFloatArgument();
     float molecular_mass_kDa    = my_current_job.arguments[6].ReturnFloatArgument();
     wxString symmetry           = my_current_job.arguments[7].ReturnStringArgument();
-	int	 num_threads			= my_current_job.arguments[8].ReturnIntegerArgument();
+	float original_pixel_size	= my_current_job.arguments[8].ReturnFloatArgument();
+	int	 num_threads			= my_current_job.arguments[9].ReturnIntegerArgument();
+
+	//need to read halfmap1 and halfmap2 into an image (read slices)
+	//so have to get num slices then do read 1..num slices into an image 
+	//that image is a density map, then combine and divide by 2 then use that 
+
+	//get num slices (may need to use MRCFile?)
+	ImageFile half_map_1_imagefile(half_map_1.ToStdString());
+	int num_slices_half_map_1 = half_map_1_imagefile.ReturnNumberOfSlices();
+	ImageFile half_map_2_imagefile(half_map_2.ToStdString());
+	int num_slices_half_map_2 = half_map_2_imagefile.ReturnNumberOfSlices();
+
+	//read slices 
+	Image half_map_1_image; 
+	Image half_map_2_image;
+	half_map_1_image.ReadSlices(&half_map_1_imagefile, 1, num_slices_half_map_1);
+	half_map_2_image.ReadSlices(&half_map_2_imagefile, 1, num_slices_half_map_2);
+
+	//combie the two image files 
+	Image combined_images(half_map_1_image);
+	combined_images.AddImage(&half_map_2_image);
+	combined_images.DivideByConstant(2); //this is now a density map? 
 
 	float		mask_falloff = 10.0;
 
 	if (is_running_locally == false) num_threads = number_of_threads_requested_on_command_line;
 
     //uh? dump file? user input? evrything until END might be gargbage// yea this is probably all grarbage  
-    ReconstructedVolume output_3d(molecular_mass_kDa);
-	ReconstructedVolume output_3d1(molecular_mass_kDa);
-	ReconstructedVolume output_3d2(molecular_mass_kDa);
+    // ReconstructedVolume output_3d(molecular_mass_kDa);
+	// ReconstructedVolume output_3d1(molecular_mass_kDa);
+	// ReconstructedVolume output_3d2(molecular_mass_kDa);
 
-	int			logical_x_dimension;
-	int			logical_y_dimension;
-	int			logical_z_dimension;
-	int			original_x_dimension;
-	int			original_y_dimension;
-	int			original_z_dimension;
-	int			images_processed;
-	float		pixel_size;
-	float		original_pixel_size;  
-	float		average_occupancy;
-	float		average_sigma;
-	float		sigma_bfactor_conversion;
-	wxString	my_symmetry;
-	bool		insert_even;
-	bool		center_mass;
-	wxString dump_file = "dump_file.dat";
+	// int			logical_x_dimension;
+	// int			logical_y_dimension;
+	// int			logical_z_dimension;
+	// int			original_x_dimension;
+	// int			original_y_dimension;
+	// int			original_z_dimension;
+	// int			images_processed;
+	// float		pixel_size;
+	// float		original_pixel_size;  
+	// float		average_occupancy;
+	// float		average_sigma;
+	// float		sigma_bfactor_conversion;
+	// wxString	my_symmetry;
+	// bool		insert_even;
+	// bool		center_mass;
+	// wxString dump_file = "dump_file.dat";
 
-	Reconstruct3D temp_reconstruction;
-	temp_reconstruction.ReadArrayHeader(dump_file, logical_x_dimension, logical_y_dimension, logical_z_dimension,
-			original_x_dimension, original_y_dimension, original_z_dimension, images_processed, pixel_size, original_pixel_size,
-			average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry, insert_even, center_mass);
+	// Reconstruct3D temp_reconstruction;
+	// temp_reconstruction.ReadArrayHeader(dump_file, logical_x_dimension, logical_y_dimension, logical_z_dimension,
+	// 		original_x_dimension, original_y_dimension, original_z_dimension, images_processed, pixel_size, original_pixel_size,
+	// 		average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry, insert_even, center_mass);
 
-	temp_reconstruction.Init(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion);
-	Reconstruct3D my_reconstruction_1(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry);
-	Reconstruct3D my_reconstruction_2(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry);
+	// temp_reconstruction.Init(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion);
+	// Reconstruct3D my_reconstruction_1(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry);
+	// Reconstruct3D my_reconstruction_2(logical_x_dimension, logical_y_dimension, logical_z_dimension, pixel_size, average_occupancy, average_sigma, sigma_bfactor_conversion, my_symmetry);
 
-	output_3d1.FinalizeSimple(my_reconstruction_1, original_x_dimension, original_pixel_size, pixel_size,
-			inner_mask_radius, outer_mask_radius, mask_falloff, half_map_1);
-	output_3d2.FinalizeSimple(my_reconstruction_2, original_x_dimension, original_pixel_size, pixel_size,
-			inner_mask_radius, outer_mask_radius, mask_falloff, half_map_2);
+	// output_3d1.FinalizeSimple(my_reconstruction_1, original_x_dimension, original_pixel_size, pixel_size,
+	// 		inner_mask_radius, outer_mask_radius, mask_falloff, half_map_1);
+	// output_3d2.FinalizeSimple(my_reconstruction_2, original_x_dimension, original_pixel_size, pixel_size,
+	// 		inner_mask_radius, outer_mask_radius, mask_falloff, half_map_2);
 
-    // END flag 
+    // // END flag 
 
     //initialize images
     Image local_resolution_volume;
-    local_resolution_volume.Allocate(output_3d.density_map->logical_x_dimension, output_3d.density_map->logical_y_dimension, output_3d.density_map->logical_z_dimension);
+    local_resolution_volume.Allocate((&combined_images)->logical_x_dimension, (&combined_images)->logical_y_dimension, (&combined_images)->logical_z_dimension);
 	local_resolution_volume.SetToConstant(0.0f);
 
     Image local_resolution_volume_all;
-	local_resolution_volume_all.Allocate(output_3d.density_map->logical_x_dimension, output_3d.density_map->logical_y_dimension, output_3d.density_map->logical_z_dimension);
+	local_resolution_volume_all.Allocate((&combined_images)->logical_x_dimension, (&combined_images)->logical_y_dimension, (&combined_images)->logical_z_dimension);
 	local_resolution_volume_all.SetToConstant(0.0f);
 
     //declare and initialize constants and other variables 
@@ -134,15 +157,15 @@ bool Generate_Local_Res_App::DoCalculation()
         Image input_volume_one_local;
         Image input_volume_two_local;
 
-        input_volume_one_local.CopyFrom(output_3d1.density_map); //maybe should use half_map_1? is it possible to get it directly? seems like im doing extra work rebuilding it?  
-        input_volume_two_local.CopyFrom(output_3d2.density_map);
+        input_volume_one_local.CopyFrom(&combined_images); 
+        input_volume_two_local.CopyFrom(&combined_images);
 
         Image mask_image;
-        mask_image.CopyFrom(output_3d.density_map);
+        mask_image.CopyFrom(&combined_images);
         float original_average_value = mask_image.ReturnAverageOfRealValues(outer_mask_radius / original_pixel_size, true);
         mask_image.ConvertToAutoMask(original_pixel_size, outer_mask_radius, original_pixel_size * 2.0f, 0.2f);
 
-        local_resolution_volume_local.Allocate(output_3d.density_map->logical_x_dimension, output_3d.density_map->logical_y_dimension, output_3d.density_map->logical_z_dimension);
+        local_resolution_volume_local.Allocate((&combined_images)->logical_x_dimension, (&combined_images)->logical_y_dimension, (&combined_images)->logical_z_dimension);
         local_resolution_volume_local.SetToConstant(0.0f);
 
         LocalResolutionEstimator *estimator = new LocalResolutionEstimator();

327fa4153c077d4df4ee72ba8caaf782f6de2383
Will Woods <willwoods@Wills-MacBook-Pro.local>
2021-06-23 19:00:24 +0000

    generates volume

--- src/programs/generate_local_res/generate_local_res.cpp
+++ src/programs/generate_local_res/generate_local_res.cpp
@@ -19,6 +19,7 @@ void Generate_Local_Res_App::DoInteractiveUserInput()
     wxString half_map_1;
     wxString half_map_2;
 	wxString output_reconstruction;
+	wxString mask_image; 
     int first_slice;
 	int last_slice;
     float pixel_size;
@@ -35,6 +36,7 @@ void Generate_Local_Res_App::DoInteractiveUserInput()
     half_map_1 = my_input->GetFilenameFromUser("Half Map 1", "The first output 3D reconstruction, calculated from half the data", "my_reconstruction_1.mrc", false);
 	half_map_2 = my_input->GetFilenameFromUser("Half Map 2", "The second output 3D reconstruction, calculated from half the data", "my_reconstruction_2.mrc", false);
 	output_reconstruction = my_input->GetFilenameFromUser("Output reconstruction", "The final 3D reconstruction, containing all data", "my_reconstruction.mrc", false);
+	mask_image = my_input->GetFilenameFromUser("Mask Image", "The filename of the mask", "mask_image.mrc", false);
     first_slice = my_input->GetIntFromUser("Starting Slice", "The slice to start from", "1", false);
 	last_slice = my_input->GetIntFromUser("Last Slice", "The slice to end with", "1", false);
     inner_mask_radius = my_input->GetFloatFromUser("Inner mask radius (A)", "Radius of a circular mask to be applied to the center of the final reconstruction in Angstroms", "0.0", 0.0);
@@ -52,40 +54,76 @@ void Generate_Local_Res_App::DoInteractiveUserInput()
 
     delete my_input;
 
-    my_current_job.ManualSetArguments("tttiiffftfif", half_map_1.ToUTF8().data(), half_map_2.ToUTF8().data(), output_reconstruction.ToUTF8().data(), first_slice, last_slice, inner_mask_radius, outer_mask_radius, molecular_mass_kDa, symmetry.ToUTF8().data(), pixel_size, number_of_threads, measured_global_resolution);
+    my_current_job.ManualSetArguments("ttttiiffftfif", half_map_1.ToUTF8().data(), half_map_2.ToUTF8().data(), output_reconstruction.ToUTF8().data(), mask_image.ToUTF8().data(), first_slice, last_slice, inner_mask_radius, outer_mask_radius, molecular_mass_kDa, symmetry.ToUTF8().data(), pixel_size, number_of_threads, measured_global_resolution);
 
 }
 
 bool Generate_Local_Res_App::DoCalculation() 
 {
     //TODO
-	// 4. pixel size vs original pixel size
+	// need to read in slices that are relavant 
+	// need to ask for mask file name, and only right slices of mask 
     wxString half_map_1					= my_current_job.arguments[0].ReturnStringArgument();
 	wxString half_map_2					= my_current_job.arguments[1].ReturnStringArgument();
 	wxString output_reconstruction 		= my_current_job.arguments[2].ReturnStringArgument();
-    int first_slice			    		= my_current_job.arguments[3].ReturnIntegerArgument();
-	int last_slice			    		= my_current_job.arguments[4].ReturnIntegerArgument();
-    float inner_mask_radius     		= my_current_job.arguments[5].ReturnFloatArgument();
-    float outer_mask_radius     		= my_current_job.arguments[6].ReturnFloatArgument();
-    float molecular_mass_kDa    		= my_current_job.arguments[7].ReturnFloatArgument();
-    wxString symmetry           		= my_current_job.arguments[8].ReturnStringArgument();
-	float original_pixel_size			= my_current_job.arguments[9].ReturnFloatArgument();
-	int	 num_threads					= my_current_job.arguments[10].ReturnIntegerArgument();
-	float measured_global_resolution 	= my_current_job.arguments[11].ReturnFloatArgument();
+	wxString mask_image_name 			= my_current_job.arguments[3].ReturnStringArgument();
+    int first_slice			    		= my_current_job.arguments[4].ReturnIntegerArgument();
+	int last_slice			    		= my_current_job.arguments[5].ReturnIntegerArgument();
+    float inner_mask_radius     		= my_current_job.arguments[6].ReturnFloatArgument();
+    float outer_mask_radius     		= my_current_job.arguments[7].ReturnFloatArgument();
+    float molecular_mass_kDa    		= my_current_job.arguments[8].ReturnFloatArgument();
+    wxString symmetry           		= my_current_job.arguments[9].ReturnStringArgument();
+	float original_pixel_size			= my_current_job.arguments[10].ReturnFloatArgument();
+	int	 num_threads					= my_current_job.arguments[11].ReturnIntegerArgument();
+	float measured_global_resolution 	= my_current_job.arguments[12].ReturnFloatArgument();
 
-	//get num slices (may need to use MRCFile?)
+	int max_width = ceil(18 / original_pixel_size);
+	int num_slices = 0;
+
+
 	ImageFile half_map_1_imagefile(half_map_1.ToStdString());
-	int num_slices_half_map_1 = half_map_1_imagefile.ReturnNumberOfSlices();
 	ImageFile half_map_2_imagefile(half_map_2.ToStdString());
-	int num_slices_half_map_2 = half_map_2_imagefile.ReturnNumberOfSlices();
+	ImageFile mask_image_imagefile(mask_image_name.ToStdString());
+	int num_slices_half_map_1 = half_map_1_imagefile.ReturnNumberOfSlices();
 
-	//read slices 
+	//read slices (keeping in mind window) 
 	Image half_map_1_image; 
 	Image half_map_2_image;
-	half_map_1_image.ReadSlices(&half_map_1_imagefile, 1, num_slices_half_map_1);
-	half_map_2_image.ReadSlices(&half_map_2_imagefile, 1, num_slices_half_map_2);
+	Image mask_image; 
 
-	//combie the two image files 
+	if (first_slice - max_width < 1) 
+	{
+		if (last_slice + max_width > num_slices_half_map_1) {
+			half_map_1_image.ReadSlices(&half_map_1_imagefile, 1, num_slices_half_map_1);
+			half_map_2_image.ReadSlices(&half_map_2_imagefile, 1, num_slices_half_map_1);
+			mask_image.ReadSlices(&mask_image_imagefile, 1, num_slices_half_map_1);  
+			num_slices = num_slices_half_map_1;
+		} else {
+			half_map_1_image.ReadSlices(&half_map_1_imagefile, 1, last_slice + max_width);
+			half_map_2_image.ReadSlices(&half_map_2_imagefile, 1, last_slice + max_width);
+			mask_image.ReadSlices(&mask_image_imagefile, 1, last_slice + max_width);
+			num_slices =  last_slice + max_width;
+		}
+		
+	} else {
+		if (last_slice + max_width > num_slices_half_map_1) { 
+			half_map_1_image.ReadSlices(&half_map_1_imagefile, first_slice - max_width, num_slices_half_map_1);
+			half_map_2_image.ReadSlices(&half_map_2_imagefile, first_slice - max_width, num_slices_half_map_1);
+			mask_image.ReadSlices(&mask_image_imagefile, first_slice - max_width, num_slices_half_map_1);
+			num_slices = num_slices_half_map_1 - (first_slice - max_width) + 1;
+		} else {
+			half_map_1_image.ReadSlices(&half_map_1_imagefile, first_slice - max_width, last_slice + max_width);
+			half_map_2_image.ReadSlices(&half_map_2_imagefile, first_slice - max_width, last_slice + max_width);
+			mask_image.ReadSlices(&mask_image_imagefile, first_slice - max_width, last_slice + max_width);
+			num_slices = last_slice + max_width - (first_slice - max_width) + 1;
+		} 
+	}
+
+	if (num_threads > num_slices) {
+		num_threads = num_slices;
+	}
+
+	//combie the two image files (need to do this in gui for correct mask)
 	Image combined_images(half_map_1_image);
 	combined_images.AddImage(&half_map_2_image);
 	combined_images.DivideByConstant(2); 
@@ -117,10 +155,10 @@ bool Generate_Local_Res_App::DoCalculation()
     int number_averaged = 0;
 	float alignment_res = 5.0f;
 
-	Image mask_image;
-	mask_image.CopyFrom(&combined_images);
+	// Image mask_image;
+	// mask_image.CopyFrom(&combined_images);
 	float original_average_value = mask_image.ReturnAverageOfRealValues(outer_mask_radius / original_pixel_size, true);
-	mask_image.ConvertToAutoMask(original_pixel_size, outer_mask_radius, original_pixel_size * 2.0f, 0.2f);
+	// mask_image.ConvertToAutoMask(original_pixel_size, outer_mask_radius, original_pixel_size * 2.0f, 0.2f);
 
 	int first_slice_with_data;
 	int last_slice_with_data;
@@ -168,8 +206,8 @@ bool Generate_Local_Res_App::DoCalculation()
 		#pragma omp parallel default(shared) num_threads(num_threads)
 		{
 
-			int first_slice = (first_slice_with_data - 1) + myroundint(ReturnThreadNumberOfCurrentThread() * slices_per_thread) + 1;
-			int last_slice = (first_slice_with_data - 1) +  myroundint((ReturnThreadNumberOfCurrentThread() + 1) * slices_per_thread);
+			int first_slice_p = (first_slice_with_data - 1) + myroundint(ReturnThreadNumberOfCurrentThread() * slices_per_thread) + 1;
+			int last_slice_p = (first_slice_with_data - 1) +  myroundint((ReturnThreadNumberOfCurrentThread() + 1) * slices_per_thread);
 
 			Image local_resolution_volume_local;
 			Image input_volume_one_local;
@@ -182,7 +220,7 @@ bool Generate_Local_Res_App::DoCalculation()
 			local_resolution_volume_local.SetToConstant(0.0f);
 
 			LocalResolutionEstimator *estimator = new LocalResolutionEstimator();
-			estimator->SetAllUserParameters(&input_volume_one_local, &input_volume_two_local, &mask_image, first_slice, last_slice, 1, original_pixel_size, box_size, threshold_snr, threshold_confidence, use_fixed_threshold, fixed_fsc_threshold,symmetry,true,2); //paralelization bug here TODO
+			estimator->SetAllUserParameters(&input_volume_one_local, &input_volume_two_local, &mask_image, first_slice_p, last_slice_p, 1, original_pixel_size, box_size, threshold_snr, threshold_confidence, use_fixed_threshold, fixed_fsc_threshold,symmetry,true,2);
 			estimator->EstimateLocalResolution(&local_resolution_volume_local);
 			delete estimator;
 
@@ -197,6 +235,7 @@ bool Generate_Local_Res_App::DoCalculation()
 		} 
 
 		float max_res = local_resolution_volume.ReturnMaximumValue();
+		MyPrintfRed("Max_Res: %f", max_res);
 
 		for (long pixel_counter = 0; pixel_counter < local_resolution_volume.real_memory_allocated; pixel_counter++)
 		{
@@ -213,8 +252,7 @@ bool Generate_Local_Res_App::DoCalculation()
 	local_resolution_volume_all.DivideByConstant(number_averaged);
 	local_resolution_volume.CopyFrom(&local_resolution_volume_all);
 
-	//local_resolution_volume.WriteSlicesAndFillHeader(output_reconstruction.ToStdString(), original_pixel_size);
-
+	local_resolution_volume.WriteSlicesAndFillHeader("local_res_vol_test.mrc", original_pixel_size);
 
 	//scaling code 
 
@@ -305,15 +343,11 @@ bool Generate_Local_Res_App::DoCalculation()
 	}
 
 	//output_3d.density_map->SetMinimumValue(original_average_value);
-
-	//  MAKE BACKGROUND ZERO - MIGHT BE BAD!!!
-	//output_3d.density_map->AddConstant(-original_average_value);
-
-
 	combined_images.CosineMask(outer_mask_radius / original_pixel_size, 1.0, false, true, 0.0);
 
+	//passing back more than asked for! TODO 
+	//combined_images.QuickAndDirtyWriteSlices(output_reconstruction.ToStdString(), first_slice, last_slice);
 	combined_images.WriteSlicesAndFillHeader(output_reconstruction.ToStdString(), original_pixel_size);
-
 
 	return true;
 }

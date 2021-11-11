c0669903881fe7306e6d913250624cccc3d1075e
WilliamWoods <47872796+WilliamWoods@users.noreply.github.com>
2021-11-11 19:25:16 +0000

    bug fixed, moving to testing

--- src/gui/ShowTemplateMatchResultsPanel.cpp
+++ src/gui/ShowTemplateMatchResultsPanel.cpp
@@ -47,7 +47,7 @@ void ShowTemplateMatchResultsPanel::OnSavePeaksClick( wxCommandEvent& event )
 		wxDateTime wxdatetime_of_run;
 		wxdatetime_of_run.SetFromDOS((unsigned long) current_result.datetime_of_run);
 
-		coordinate_file.WriteCommentLine(wxString::Format("Template Match Result #%li (%s - %s, %s)", current_result.job_name, wxdatetime_of_run.FormatISODate(), wxdatetime_of_run.FormatISOTime()).ToUTF8().data());
+		coordinate_file.WriteCommentLine(wxString::Format("Template Match Result #%li (%s - %s, %s)", current_result.job_id, current_result.job_name, wxdatetime_of_run.FormatISODate(), wxdatetime_of_run.FormatISOTime()).ToUTF8().data());
 		coordinate_file.WriteCommentLine(wxString::Format("Searched Image               : %s", image_asset_panel->ReturnAssetLongFilename(image_asset_panel->ReturnArrayPositionFromAssetID(current_result.image_asset_id))).ToUTF8().data());
 		coordinate_file.WriteCommentLine(wxString::Format("Reference Volume File        : %s", volume_asset_panel->ReturnAssetLongFilename(volume_asset_panel->ReturnArrayPositionFromAssetID(current_result.ref_volume_asset_id))).ToUTF8().data());
 		coordinate_file.WriteCommentLine(wxString::Format("Used Result Threshold        : %.2f", current_result.used_threshold).ToUTF8().data());

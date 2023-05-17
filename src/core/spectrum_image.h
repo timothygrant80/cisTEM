ec3ffb1ad753ef7d6d6c51858d0f8ec0824341ea
Johannes Elferich <jojotux123@hotmail.com>
2023-05-17 13:13:54 +0000

    Need this here, too

--- src/core/spectrum_image.h
+++ src/core/spectrum_image.h
@@ -4,6 +4,8 @@
 
 */
 
+#define use_epa_rather_than_zero_counting
+
 class SpectrumImage : public Image {
   public:
     float FindRotationalAlignmentBetweenTwoStacksOfImages(Image* other_image, int number_of_images, float search_half_range, float search_step_size, float minimum_radius, float maximum_radius);

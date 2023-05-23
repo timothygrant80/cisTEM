e2b327ef242c41d964b9c473e4ccda1afbe9e94c
himesb <himes.benjamin@gmail.com>
2023-05-23 11:35:15 -0400

    SAFE TO IGNORE: modifies personal settings files.

--- scripts/containers/top_image/install_wx_3.0.5_dynamic.sh
+++ scripts/containers/top_image/install_wx_3.0.5_dynamic.sh
@@ -10,10 +10,10 @@ cd /opt/WX/intel-dynamic/wxWidgets-3.0.5 && make install && make clean && ldconf
 rm -rf /opt/WX/intel-dynamic/wxWidgets-3.0.5
 
 # First noticed outside container with g++9, several errors in longlong.h seem to be fixed by this extra include  /usr/include/wx-3.1-unofficial
-# tf=`tempfile` && cp /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h.orig && \
-#     awk '{if(/#include "wx\/defs.h"/){ print $0 ;print "#include <wx/txtstrm.h>"} else print $0}' /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h.orig > $tf && \
-#     mv $tf /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h && \
-#     chmod a+r /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h
+tf=`tempfile` && cp /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h.orig && \
+    awk '{if(/#include "wx\/defs.h"/){ print $0 ;print "#include <wx/txtstrm.h>"} else print $0}' /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h.orig > $tf && \
+    mv $tf /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h && \
+    chmod a+r /opt/WX/intel-dynamic/include/wx-3.0/wx/longlong.h
 
 
 

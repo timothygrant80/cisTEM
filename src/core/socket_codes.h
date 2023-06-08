7e6cb86b4aa3b2af2e5f360ebcc7547aa500721c
Johannes Elferich <jojotux123@hotmail.com>
2023-06-08 18:38:21 +0000

    Remove remote control

--- src/core/socket_codes.h
+++ src/core/socket_codes.h
@@ -25,5 +25,4 @@ const unsigned char socket_result_with_image_to_write[]  = "=z4-Y8Ge?vEjh`H^";
 const unsigned char socket_program_defined_result[]      = "e}w<S9hm<3L6Dr+V";
 const unsigned char socket_send_thread_timing[]          = "Kq04etrq1fO2QV4d";
 const unsigned char socket_template_match_result_ready[] = "EP927e$*cQ^egWq'";
-const unsigned char socket_remote_control[]              = "9^$[9^$[9^$[9^$[";
 #endif

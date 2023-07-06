0459fc805519821c57a35c1097d55c019a0be888
Johannes Elferich <jojotux123@hotmail.com>
2023-07-06 17:28:01 +0000

    Fix header file

--- src/core/core_headers.h
+++ src/core/core_headers.h
@@ -170,7 +170,6 @@ class StackDump : public wxStackWalker // so we can give backtraces..
 #include "json/jsonval.h"
 #include "stopwatch.h"
 #include "ccl3d.h"
-#include "orientation_cluster.h"
 
 #ifdef EXPERIMENTAL
 #include "../../include/ieee-754-half/half.hpp"

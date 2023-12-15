8ceb0eb2b8856cd085b661fd188c88bfc72c97d8
Tim <timwagner240@gmail.com>
2023-12-15 07:36:00 -0600

    New display now opens on cisTEM button clicks
    Added the ability for command line arguments to be passed.
    This enables proper display of images/stacks when using various buttons
    for display within the main cisTEM program.

--- src/programs/cisTEM_display/cisTEM_display.cpp
+++ src/programs/cisTEM_display/cisTEM_display.cpp
@@ -4,6 +4,8 @@ class DisplayApp : public wxApp {
   public:
     virtual bool OnInit( );
     virtual int  OnExit( );
+    virtual void OnInitCmdLine(wxCmdLineParser& parser);
+    virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
 };
 
 IMPLEMENT_APP(DisplayApp)
@@ -12,9 +14,31 @@ DisplayFrame* display_frame;
 
 bool DisplayApp::OnInit( ) {
     wxInitAllImageHandlers( );
+
     display_frame = new DisplayFrame(NULL, wxID_ANY, "cisTEM Display", wxPoint(-1, -1), wxSize(-1, -1), wxDEFAULT_FRAME_STYLE);
+    wxString cmd_full_filename;
+    wxString cmd_filename;
+
+    // Check if filenames are present; if so, open them
+    if ( argc > 1 ) {
+        for ( int i = 1; i < argc; i++ ) {
+            cmd_filename = argv[i];
+            wxFileName filename(cmd_filename);
+            filename.Normalize(wxPATH_NORM_LONG | wxPATH_NORM_DOTS | wxPATH_NORM_TILDE | wxPATH_NORM_ABSOLUTE);
+            cmd_full_filename = filename.GetFullPath( );
+            display_frame->cisTEMDisplayPanel->OpenFile(cmd_full_filename, cmd_full_filename);
+        }
+    }
+
     display_frame->Layout( );
     display_frame->Show(true);
+    return true;
+}
+
+void DisplayApp::OnInitCmdLine(wxCmdLineParser& parser) {
+}
+
+bool DisplayApp::OnCmdLineParsed(wxCmdLineParser& parser) {
 
     return true;
 }
